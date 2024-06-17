aef03122924ae8c2012e9bde8939503370157569
marchiorijulia <juliamarchioriramos@gmail.com>
2024-06-17 10:09:53 -0300

    atualizações

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -2,14 +2,7 @@ const {Router} = require('express');
 const router = Router();
 
 const {storeTask} = require('../controller/task.Controller');
-<<<<<<< HEAD
 
 router.post('/store/task', storeTask);
-=======
-const {storePost} = require('../controller/task.Controller');
-
-router.post('/store/task', storeTask);
-router.post('/store/task', storePost);
->>>>>>> fd3e9e72dcd8aa70484fb34c9b1e1e07f52ecdba
 
 module.exports = router;
