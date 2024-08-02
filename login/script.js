76152d7b904f73b074b9ba98d2fbabc4549720db
JULIA MARCHIORI RAMOS <05334254099@senacrs.edu.br>
2024-08-02 12:14:33 -0300

    atualizações

--- login/script.js
+++ login/script.js
@@ -5,17 +5,20 @@ button.onclick = async function(){
     let senha = document.getElementById('senha').value;
     let data = {email,senha}
     
+    console.log("aqui")
     const response = await fetch('http://localhost:3000/api/store/login', {
         method: 'POST',
         headers: {'Content-type': "application/json;charset=UTF-8"},
         body: JSON.stringify(data)
     });
 
+    console.log("aqui2")
+
     let content = await response.json();
     console.log(content)
     if(content.success){
         alert('Usuário logado.');
     }else{
-        alert('Ocorreu um erro');
+        alert(content.msg);
     }
 }
